f80cbe60d5066c331718a4dafee11bd2c7eb23c0
Coding-Machina <harikrishnanhk.1996@gmail.com>
2018-08-31 17:58:22 +0530

    Initial Commit
    First Upload

---
+++ CorsEgMVC/CorsEg/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace CorsEg.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ CorsEgMVC/CorsEg/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CorsEg.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
